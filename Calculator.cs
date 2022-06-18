c1b2a7036e7957df98cdb04dc782b5a8d74d9912
Yugenzariah <KeithJhaeron-Cayatoc@live.nmit.ac.nz>
2022-06-18 12:58:16 +1200

    Translated JavaScript.cs

--- Calculator.cs
+++ Calculator.cs
@@ -10,19 +10,19 @@ namespace Calculator
             Console.WriteLine("Enter another number: "); //Number two entry
             double num2 = Convert.ToDouble(Console.ReadLine()); //Converting number 2 to a double
             Console.WriteLine("Choose an operator from the following list:"); //Choosing an operator
-            Console.WriteLine("\ta - Add"); //Entering the string "a" will make it Add the two numbers and so on 
-            Console.WriteLine("\ts - Subtract");
-            Console.WriteLine("\tm - Multiply");
-            Console.WriteLine("\td - Divide");
-            Console.WriteLine("\tp - Power");
-            Console.WriteLine("\tr - Remainder");
-            Console.WriteLine("Enter your operator: ");
+            Console.WriteLine("\ta - Add"); // Putting the input string "a" will add the numbers
+            Console.WriteLine("\ts - Subtract"); // Putting the input string "s" will subract the numbers
+            Console.WriteLine("\tm - Multiply"); // Putting the input string "m" will multiply the numbers
+            Console.WriteLine("\td - Divide"); // Putting the input string "d" will multiply the numbers
+            Console.WriteLine("\tp - Power"); // Putting the input string "p" will find you the result of a power of a number
+            Console.WriteLine("\tr - Remainder"); // Putting the input string "r" will subtract the whole number then multiply the decimal value
+            Console.WriteLine("Enter your operator: "); 
             string op = Console.ReadLine(); //Reads the string entered and saves it as a string 
             double result = 0; //Sets a double of result to 0 to be further modified
             switch (op) //Switch statement for the previously entered string
             {
                 case "a":
-                    result = num1 + num2; //E.G when "a" is entered it will perform this equation 
+                    result = num1 + num2;
                     break;
                 case "s":
                     result = num1 - num2;
@@ -40,10 +40,10 @@ namespace Calculator
                     result = num1 % num2;
                     break;
                 default:
-                    Console.WriteLine("Invalid operator input"); //If the input is any different from the inputs provided it will show this error
+                    Console.WriteLine("Invalid operator input"); // If the inout is different from the inputs privided it will prompt you an erroroperator input"
                     break;
             }
-            Console.WriteLine("Your result is: "); //It now prints the answer
+            Console.WriteLine("Your result is: "); // It now prints the answer
             Console.WriteLine(result);
         }
     }
---
+++ Translated JavaScript.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class exampleJavaScript
+{
+    void exampleJavaScript(arr)
+    {
+        let newArr = [];
+        for (let i = 0; i < arr.length; i++)
+        {
+            let largest = arr[i][0];
+            for (let j = 0; j < arr[i].length; j++) 
+            {
+                if (arr[i][j] > largest)
+                {
+                    largest = arr[i][j]
+                }
+            }
+            newArr.push(largest)
+        }
+        return newArr
+    }
+
+    Console.WriteLine(exampleJavaScript)
+}
